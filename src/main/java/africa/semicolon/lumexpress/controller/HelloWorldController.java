d81e5394621a77f03eb8bc222c767c9edccadd24
Adeweh <dorcasabang@gmail.com>
2022-09-21 16:30:30 +0100

    Initial commit

---
+++ src/main/java/africa/semicolon/lumexpress/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package africa.semicolon.lumexpress.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+    @GetMapping("/ijola")
+    public String Welcome(){
+        return "Hello World Again";
+    }
+}
+
